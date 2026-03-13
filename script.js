84675784982c264b2ed91e04e3a5b377edd2683c
ejaymoreno0-hue <ejaymoreno0@gmail.com>
2026-03-14 02:28:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Fade animation on page load
+
+window.addEventListener("load", () => {
+
+document.body.style.opacity = "0";
+
+setTimeout(()=>{
+document.body.style.transition = "1s";
+document.body.style.opacity = "1";
+},200);
+
+});
